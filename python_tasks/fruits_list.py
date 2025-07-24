1b5d1b35daea0f459ba56f5d1a1df7e0ec8f6095
iiceekiing <iiceekiingsocials@gmail.com>
2025-07-24 11:10:42 +0100

    add: fruits_list.py for practicing list operations

---
+++ python_tasks/fruits_list.py
@@ -0,0 +1,47 @@
+#A list is an ordered, mutable (changeable) collection of elements in Python.
+
+fruits = ['apple', 'banana', 'cherry']
+
+
+
+"""
+✅ Features:
+Ordered (keeps insertion order)
+
+Allows duplicates
+
+Mutable (can change values)
+
+Can store any data type (mixing allowed)
+
+"""
+
+
+#🔧 Common Operations
+
+#1. Accessing Elements (Indexing)
+
+print(fruits[0])      # 'apple'
+print(fruits[-1])     # 'cherry'
+
+
+#2. Slicing
+
+print(fruits[0:2])    # ['apple', 'banana']
+print(fruits[::2])    # ['apple', 'cherry']
+
+
+#3. Modifying Lists
+
+fruits[1] = 'mango'
+print(fruits)
+fruits.append('orange')
+print(fruits)
+fruits.insert(1, 'kiwi')
+print(fruits)
+fruits.pop() # Removes last item
+print(fruits)
+fruits.remove('kiwi') # Removes by value
+print(fruits)
+
+
