70adfeea34e66d960add2831277e31f79aa20974
iiceekiing <iiceekiingsocials@gmail.com>
2025-07-24 10:58:56 +0100

    add: list.py for practicing list operations in python; like list:  methods, indexing, slicing, etc

---
+++ python_tasks/list.py
@@ -0,0 +1,5 @@
+numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("New list content: ", numbers)  # Current list content.
